40e0a77dc950892ac9b5f83478d6c174117328d9
Turan Öz <turanozx@gmail.com>
2021-11-26 17:11:39 +0300

    birimler update

---
+++ js/birimler.js
@@ -0,0 +1,52 @@
+let birimler = [
+    {
+        birim: "Banka Hesap Bilgileri",
+        bankalar: [
+            {
+                img: "/img/banka_enpara_sirketim-1.png)",
+                alici: "YT Bilişim Hizmetleri Sanayi ve Ticaret Limited Şirketi",
+                IBAN: "TR98 0000 0052 0003 2089 02",
+                hesapNo: "67869855",
+                sube: "036352",
+                birim: "Türk Lirası"
+            },
+            {
+                img: "/img/banka_yapikredi.png)",
+                alici: "YT Bilişim Hizmetleri Sanayi ve Ticaret Limited Şirketi",
+                IBAN: "TR87 0000 0044 0003 2525 02",
+                hesapNo: "6756895",
+                sube: "036568",
+                birim: "Türk Lirası"
+            },
+            {
+                img: "/img/banka_ziraat.png)",
+                alici: "YT Bilişim Hizmetleri Sanayi ve Ticaret Limited Şirketi",
+                IBAN: "TR44 2000 0052 0003 2459 99",
+                hesapNo: "1286999",
+                sube: "012522",
+                birim: "Türk Lirası"
+            },
+            {
+                img: "/img/garanti_bbva.png)",
+                alici: "YT Bilişim Hizmetleri Sanayi ve Ticaret Limited Şirketi",
+                IBAN: "TR72 0000 0052 0003 2088 45",
+                hesapNo: "57895525",
+                sube: "369524",
+                birim: "Türk Lirası"
+            }
+        ]
+    },
+    {
+        birim: "İnsan Kaynakları,
+        nedenbiz: "Dünya lideri teknoloji firmaları ile üst düzeyde iş ortaklığı yaparız.\n" +
+            "İnovatif fikirlere ve ARGE çalışmalarına yatırım yaparak yeni teknolojileri hep bir adım önde sunarız.\n" +
+            "Uluslararası geçerliliğe sahip belgelendirmeler ile hizmet kalitesini sürekli güçlendiririz.\n" +
+            "Referans niteliğindeki bulut, danışmanlık, altyapı, yazılım projelerine imza atarız.\n" +
+            "Çalışanlarımızın tüm yasal haklarını korur ve takip ederiz.\n" +
+            "Güvenli ve sağlıklı çalışma ortamı yaratmak için İşçi Sağlığı ve İş Güvenliği uygulamalarına önem veririz.\n" +
+            "Çalışanlarımıza farklı projelerde görev alma şansı vererek, bireysel gelişimlerine katkı sağlarız.\n" +
+            "Organizasyonel yapılanmayı ihtiyaçlara ve değişime göre şekillendiririz.\n" +
+            "Her zaman çalışanlarımızın yanında olur, değerleriyle hareket ederiz.\n" +
+            "Başarıya ulaşmadaki en önemli etkenin çalışanlarımız olduğunun bilinciyle davranırız"
+    }
+]
